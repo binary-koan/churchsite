7ae6720f5d4ebd891c249db7636ac0ff02119428
Jono Mingard <reason.koan@gmail.com>
2014-03-21 21:59:17 +1300

    Changed uploads to be stored in data dir

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,7 +17,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    if ENV['OPENSHIFT_DATA_DIR']
+      "#{ENV['OPENSHIFT_DATA_DIR']}uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"  
+    else
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
