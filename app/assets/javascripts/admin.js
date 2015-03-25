922f08b8c913db16cb35369499eca57ac0b6ceee
Jono Mingard <reason.koan@gmail.com>
2015-03-26 10:48:11 +1300

    Use different JS for pages and admin section

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -10,15 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//TODO make turbolinks work
-// require turbolinks
-//= require bootstrap-sprockets
-//= require bootstrap-datepicker
+//= require ./application
 //
-//= require audioplayer
-//= require date
-//= require jquery.magnific-popup
-//
-//= require_tree ./logic
+//= require bootstrap-timepicker
+//= require ckeditor/init
+//= require jquery.placeholder
+//= require sortable
