99c3a17c89d8c2550a2d836066f2d3f11b8e9d01
Jono Mingard <reason.koan@gmail.com>
2015-04-04 11:00:31 +1300

    Allow editing name under Account Settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) { |u|
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+    }
   end
 end
