c4b6a73a13f59a175e22903e0aaf0ad20c2cdeec
Jono Mingard <reason.koan@gmail.com>
2015-12-29 13:59:28 +1300

    Clean up route definitions

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,11 @@ Rails.application.routes.draw do
   get "uploads/ckeditor/pictures/:id/content_:filename", to: 'uploads#ckeditor_picture_content'
   get "uploads/ckeditor/pictures/:id/thumb_:filename", to: 'uploads#ckeditor_picture_thumb'
 
+  resources :sermons, path: "word", except: [:index]
+
   resources :news_items, path: "news", only: [:create, :update, :destroy] do
     get :events_in_month, on: :collection
   end
-
-  resources :sermons, path: "word", except: [:index]
 
   resources :photos, except: [:index, :show] do
     get :gallery, action: :gallery, on: :collection
@@ -24,24 +24,22 @@ Rails.application.routes.draw do
   end
 
   resources :churches, except: [:index, :show]
+
   resources :options, only: [:edit, :update]
+
   resources :pages, except: [:show] do
     post :reorder, on: :collection
   end
 
-  resources :community_pages do
-    get :reorder, on: :collection
-    post :reorder, on: :collection
-  end
+  scope "/admin" do
+    get "/", to: "admin#redirect"
 
-  scope '/admin' do
-    get "users", to: "admin#users"
+    get :users, to: "admin#users"
     post "users/confirm", to: "admin#confirm_user"
     delete "users/deny", to: "admin#deny_user"
-  end
 
-  get :admin, to: "admin#redirect"
-  get :toggle_editing, to: "admin#toggle_editing"
+    get :toggle_editing, to: "admin#toggle_editing"
+  end
 
   get "/:id", to: "pages#display"
 end
