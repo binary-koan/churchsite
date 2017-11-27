ae16cddcc3048da9ce9e413436626244b6628e2f
Jono Mingard <reason.koan@gmail.com>
2017-11-27 21:39:38 +1300

    Set a hostname for the mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,10 @@ Rails.application.configure do
     from: ENV['SMTP_USERNAME']
   }
 
+  config.action_mailer.default_url_options = {
+    host: ENV['PUBLIC_SITE_DOMAIN']
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
