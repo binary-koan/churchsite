31d9aef007a0e8ffc1f6983f5c069475d4ce1f8d
Jono Mingard <reason.koan@gmail.com>
2019-05-11 23:02:06 +0900

    Fix puma config in development

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,10 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+if ENV.fetch('RAILS_ENV', 'development') != 'development'
+  workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+  threads_count = Integer(ENV['MAX_THREADS'] || 5)
+  threads threads_count, threads_count
 
-preload_app!
+  preload_app!
+end
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
