fdc0e923cce639fdeda10798269ba62617eee63e
Jono Mingard <reason.koan@gmail.com>
2019-05-13 21:50:21 +0900

    Don't initialize AWS if no config is set

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,16 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['SPACES_ACCESS_KEY'] || Rails.application.credentials.spaces_access_key,
-    aws_secret_access_key: ENV['SPACES_SECRET_KEY'] || Rails.application.credentials.spaces_secret_key,
-    region:                'nyc3',
-    endpoint:              'https://nyc3.digitaloceanspaces.com'
-  }
-  config.fog_directory = ENV['SPACES_NAME'] || 'lincolnchurch-staging'
+  aws_access_key = ENV['SPACES_ACCESS_KEY'] || Rails.application.credentials.spaces_access_key
+  aws_secret_key = ENV['SPACES_SECRET_KEY'] || Rails.application.credentials.spaces_secret_key
+
+  if aws_access_key && aws_secret_key
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     aws_access_key,
+      aws_secret_access_key: aws_secret_key,
+      region:                'nyc3',
+      endpoint:              'https://nyc3.digitaloceanspaces.com'
+    }
+    config.fog_directory = ENV['SPACES_NAME'] || 'lincolnchurch-staging'
+  end
 end
